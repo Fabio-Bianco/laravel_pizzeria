db89ea71f48ea8379c39701c15ef97f20384ebb1
Fabio-Bianco <biancobfabio@gmail.com>
2025-10-08 19:22:19 +0200

    Refactoring sidebar e miglioramenti accessibilità

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,29 +1,25 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="h4 mb-0">Profilo personale</h2>
-    </x-slot>
+@extends('layouts.app-modern')
 
-    <div class="row g-3">
+@section('title', 'Profilo personale')
+
+@section('header')
+<div class="text-center py-4">
+    <div class="mb-2" style="font-size:3rem;">👤</div>
+    <h1 class="display-6 fw-bold text-dark mb-2">Profilo personale</h1>
+    <p class="lead text-muted mb-4">Gestisci i dati del tuo profilo, la password e l'account</p>
+</div>
+@endsection
+
+@section('content')
+    <div class="row g-4 justify-content-center">
         <div class="col-12 col-lg-6">
-            <div class="card shadow-sm">
-                <div class="card-body">
-                    @include('profile.partials.update-profile-information-form')
-                </div>
-            </div>
+            @include('profile.partials.update-profile-information-form')
         </div>
         <div class="col-12 col-lg-6">
-            <div class="card shadow-sm">
-                <div class="card-body">
-                    @include('profile.partials.update-password-form')
-                </div>
-            </div>
+            @include('profile.partials.update-password-form')
         </div>
-        <div class="col-12">
-            <div class="card shadow-sm">
-                <div class="card-body">
-                    @include('profile.partials.delete-user-form')
-                </div>
-            </div>
+        <div class="col-12 col-lg-8">
+            @include('profile.partials.delete-user-form')
         </div>
     </div>
-</x-app-layout>
+@endsection
